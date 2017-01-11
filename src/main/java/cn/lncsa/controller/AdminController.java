136a97f85e093e9f9c033756952cf27d1099255b
阪本先生 <lingerkong03@gmail.com>
2017-01-11 20:45:17 +0800

    新增加了点返回JSON数据的接口，测试用。

---
+++ src/main/java/cn/lncsa/controller/AdminController.java
@@ -0,0 +1,7 @@
+package cn.lncsa.controller;
+
+/**
+ * Created by catten on 1/11/17.
+ */
+public class AdminController {
+}
--- src/main/java/cn/lncsa/view/BulletinWrapper.java
+++
@@ -1,8 +0,0 @@
-package cn.lncsa.view;
-
-/**
- * Created by cattenlinger on 2017/1/6.
- */
-public class BulletinWrapper {
-
-}
